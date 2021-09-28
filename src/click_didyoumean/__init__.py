d7638142d0fa731092de14be6d6fafcbc368bd02
Timo Furrer <timo.furrer@roche.com>
2021-09-28 22:15:16 +0200

    Add typing

--- src/click_didyoumean/__init__.py
+++ src/click_didyoumean/__init__.py
@@ -4,6 +4,7 @@ with a git-like *did-you-mean* feature.
 """
 
 import difflib
+import typing
 
 import click
 
@@ -15,25 +16,29 @@ class DYMMixin:
     a certain command is not registered.
     """
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args: typing.Any, **kwargs: typing.Any) -> None:
         self.max_suggestions = kwargs.pop("max_suggestions", 3)
         self.cutoff = kwargs.pop("cutoff", 0.5)
-        super().__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)  # type: ignore
 
-    def resolve_command(self, ctx, args):
+    def resolve_command(
+        self, ctx: click.Context, args: typing.List[str]
+    ) -> typing.Tuple[
+        typing.Optional[str], typing.Optional[click.Command], typing.List[str]
+    ]:
         """
         Overrides clicks ``resolve_command`` method
         and appends *Did you mean ...* suggestions
         to the raised exception message.
         """
         try:
-            return super(DYMMixin, self).resolve_command(ctx, args)
+            return super(DYMMixin, self).resolve_command(ctx, args)  # type: ignore
         except click.exceptions.UsageError as error:
             error_msg = str(error)
             original_cmd_name = click.utils.make_str(args[0])
             matches = difflib.get_close_matches(
                 original_cmd_name,
-                self.list_commands(ctx),
+                self.list_commands(ctx),  # type: ignore
                 self.max_suggestions,
                 self.cutoff,
             )
